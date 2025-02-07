48134cdd3dd59bc1357ffe54adac4283bff44f6a
Chitra Kumar Sai Chenuri Venkata <chitrakumarsai.chenurivenkata@invista.com>
2025-02-07 08:56:35 -0600

    updating snowflake_version notebook

---
+++ data/purchasing_anomaly.sql
@@ -0,0 +1,10 @@
+select 
+    PAM.*, ZM."User Desc" 
+from 
+    ZINT_PR.UI_ALL.VW_RPT_PURCHASE_ANOMALIES_MODEL as PAM
+left join 
+    (SELECT "User ID", "User Desc" 
+        FROM "ZINT_MASTER"."UI_SEC"."AD Account") 
+    AS ZM on ZM."User ID" = PAM."Requisitioner Master"
+    where "Purchase Doc Date" >= DATEADD(DAY, -450, GETDATE())
+    LIMIT 1000;
