5a250a3fb6b72d9998c8297dfae68924ec1366f3
Chitra Kumar Sai Chenuri Venkata <chitrakumarsai.chenurivenkata@invista.com>
2025-02-13 14:08:23 -0600

    updating the snowpark script

--- data/experiment.sql
+++ data/experiment.sql
@@ -1,31 +1,13 @@
-CREATE OR REPLACE PROCEDURE send_email_with_table_attachment()
-RETURNS STRING
-LANGUAGE JAVASCRIPT
-AS 
-$$
-try {
-    // Step 1: Export table data to a staged CSV file
-    var export_query = `COPY INTO @STAGE_PURCHASING_ANOMALY/pruchasing_weekly_report.csv
-                        FROM DS_D_WORKSPACE.PURCHASING_ANOMALY.PURCHASES_ANOMALY_RESULTS_LAST_WEEK
-                        FILE_FORMAT = (TYPE = CSV, FIELD_OPTIONALLY_ENCLOSED_BY = '"')
-                        HEADER = TRUE;`;
+With purch_an as (select 
+    PAM.*, ZM."User Desc" 
+from 
+    ZINT_PR.UI_ALL.VW_RPT_PURCHASE_ANOMALIES_MODEL as PAM
+left join 
+    (SELECT "User ID", "User Desc" 
+        FROM "ZINT_MASTER"."UI_SEC"."AD Account") 
+    AS ZM on ZM."User ID" = PAM."Requisitioner Master"
+    where "Purchase Doc Date" >= DATEADD(DAY, -450, GETDATE())
+    LIMIT 1000)
 
-    snowflake.execute({sqlText: export_query});
-
-    // Step 2: Send an email with the CSV attachment
-    var email_query = `CALL SYSTEM$SEND_EMAIL(
-                           'your_email_integration',
-                           'recipient@example.com',
-                           'Snowflake Table Export',
-                           'Attached is the exported table data.',
-                           '@your_stage/your_table_export.csv'
-                       );`;
-
-    snowflake.execute({sqlText: email_query});
-
-    return "Email Sent Successfully with Table Attachment";
-} 
-catch (err) {
-    return "Error: " + err.message;
-}
-$$;
+select * from purch_an
+where "User Desc" is not null;
